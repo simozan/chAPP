afbda4a1276f55b364b27ea6caeaad089c54fb20
John Doe <johndoe@example.com>
2024-02-10 15:34:54 +0100

    End of changing morning

--- project-3-client/src/components/Entry/Entry.jsx
+++ project-3-client/src/components/Entry/Entry.jsx
@@ -1,5 +1,9 @@
 import "./Entry.css";
-function Entry() {
-    return(<h1>Entry</h1>)
+function Entry(props) {
+    return(<div>
+        <p>{props.entryClient.charAt(0).toUpperCase()+props.entryClient.slice(1)}</p>
+        <p>{props.entryQuantity}</p>
+        <p>{props.entryDate}</p>
+    </div>)
 }
 export default Entry;
--- project-3-client/src/pages/HomePage/HomePage.jsx
+++ project-3-client/src/pages/HomePage/HomePage.jsx
@@ -3,19 +3,54 @@ import { AuthContext } from "../../context/auth.context";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import entriesdatas from "./entries.json"
+import Entry from "../../components/Entry/Entry";
 function HomePage() {
-  const { user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   return (
     <div>
       <h1>Hello {user.name.charAt(0).toUpperCase() + user.name.slice(1)}</h1>
       {entriesdatas.map((eachEntry) => {
-          return ( <Entry
-            entryClient={eachEntry.entryClient}
-            entryQuantity={eachEntry.entryQuantity}
-            entryDate={eachEntry.entryDate}
-          />
-          )})}
-      <Link to="/Createentry">
+        return (
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Column 1</th>
+                  <th>Date</th>
+                  <th>Client</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Row 1</td>
+                  <td>Row 1</td>
+                  <td>Row 1</td>
+                </tr>
+                <tr>
+                  <td>Row 2</td>
+                  <td>Row 2</td>
+                </tr>
+                <tr>
+                  <td>Row 3</td>
+                  <td>Row 3</td>
+                  <td>Row 3</td>
+                </tr>
+                <tr>
+                  <td>Row 4</td>
+                  <td>Row 4</td>
+                </tr>
+              </tbody>
+            </table>
+            <Entry
+              entryClient={eachEntry.entryClient}
+              entryQuantity={eachEntry.entryQuantity}
+              entryDate={eachEntry.entryDate}
+            />
+          </div>
+        )
+      })}
+      <Link to="/CreateEntry">
         <button>Create entry</button>
       </Link>
     </div>
